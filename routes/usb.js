39f84cb3b23fd3da830dfd94ebd27baa8be72f22
dikkini <dikkini@gmail.com>
2015-04-15 00:42:17 +0300

    localhost iframe ip

--- routes/usb.js
+++ routes/usb.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usb', { title: 'Online USB Make', ip: "http://10.211.55.3:8080/" });
-  //res.render('usb', { title: 'WinUSBOnline', ip: "http://localhost:8080/" });
+  //res.render('usb', { title: 'Online USB Make', ip: "http://10.211.55.3:8080/" });
+  res.render('usb', { title: 'WinUSBOnline', ip: "http://localhost:8080/" });
 });
 
 module.exports = router;
