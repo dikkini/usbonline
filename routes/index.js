4214ffcc85d65ee8f9b557d02c13336ac0ecf7c4
dikkini <dikkini@gmail.com>
2015-04-15 00:36:56 +0300

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WinUSBOnline' });
+  res.render('index', { title: 'Online USB Make' });
 });
 
 module.exports = router;
--- routes/usb.js
+++ routes/usb.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usb', { title: 'WinUSBOnline', ip: "http://10.211.55.3:8080/" });
+  res.render('usb', { title: 'Online USB Make', ip: "http://10.211.55.3:8080/" });
   //res.render('usb', { title: 'WinUSBOnline', ip: "http://localhost:8080/" });
 });
 
