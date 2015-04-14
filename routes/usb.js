ead6758454b3b387326801ae8fda7b37f06a14e0
dikkini <dikkini@gmail.com>
2015-04-15 00:11:14 +0300

    force localhost to use http, export usb routes and change title of application

--- routes/usb.js
+++ routes/usb.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usb', { title: 'Welcome' });
+  res.render('usb', { title: 'WinUSBOnline' });
 });
 
 module.exports = router;
