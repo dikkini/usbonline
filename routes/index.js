e81bbdc2fa2fc7386de58f75820baa8d5df76554
dikkini <dikkini@gmail.com>
2015-04-25 02:42:01 +0300

    log routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
-    , router = express.Router();
+    , router = express.Router()
+    , fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +12,24 @@ router.get('/usb', function(req, res, next) {
 });
 
 router.post('/log', function(req, res, next) {
-    // TODO
+    var id = req.body.Id;
+    var log = req.body.Msg;
+    fs.writeFile("/tmp/usbonline/logs/" + id, log, function(err) {
+        if (err) {
+            return log.error(err);
+        }
+        log.info("Log done");
+    });
+});
+
+router.get('/log/:id', function(req, res, next) {
+    var id = req.params.id;
+    fs.readFile('/tmp/usbonline/logs/' + id, function (err, data) {
+        if (err) {
+            return log.error(err);
+        }
+        res.end(data);
+    });
 });
 
 module.exports = router;
