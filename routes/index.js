30d95659a23cba4dff3373c93a1c9b8b1d3d3dd7
dikkini <dikkini@gmail.com>
2015-04-27 00:37:15 +0300

    localhost_page

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/usb', function (req, res, next) {
+	res.render('usb', {title: 'Online USB Make', ip: "http://localhost:8080/"});
+});
+
+router.get('/usb1', function (req, res, next) {
 	res.render('usb', {title: 'Online USB Make', ip: "http://10.211.55.3:8080/"});
 });
 
