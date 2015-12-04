5a6d64a0fcc07b4b564f0a5327388a9ad3740fe3
Artur Karapetov <dikkini@gmail.com>
2015-12-04 12:36:11 +0300

    fixes

--- controllers/utils.js
+++ controllers/utils.js
@@ -71,7 +71,7 @@ router.post('/feedback_win', function(req, res, next) {
 	};
 
 	log.debug("Add user feedback from portable application to database");
-	db.query(config.get("sql:add_user_feedback"), [feedback_email, feedback, true, sessionid], function (err, result) {
+	db.query(config.get("sql:add_user_feedback_win"), [sessionid, feedback_email, feedback, true], function (err, result) {
 		log.debug(result);
 		log.error(err);
 		log.error("Add user feedback error: ", err);
