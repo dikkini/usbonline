147185beced47f45e2d2f9187daff4a40f88b408
Artur Karapetov <dikkini@gmail.com>
2015-12-08 02:19:29 +0300

    first page. new navbar. a lot of fonts and etc

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,11 @@ var express = require('express')
 	, log = require('../libs/log')(module);
 
 router.get('/', function (req, res, next) {
-	res.render('index_new');
+	res.render('index');
+});
+
+router.get('/app', function (req, res, next) {
+	res.render('app');
 });
 
 module.exports = router;
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,3 +1,32 @@
+$(document).ready(function() {
+	'use strict'
+
+	$(function() {
+		// Make sure that #header-background-image height is equal to the browser height.
+
+		$('header').css({ 'height': $(window).height() });
+		$('section').css({ 'height': $(window).height() });
+		$(window).on('resize', function() {
+			$('header').css({ 'height': $(window).height() });
+			$('section').css({ 'height': $(window).height() });
+			$('body').css({ 'width': $(window).width() })
+		});
+	});
+
+	$('.smoothscroll').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash,
+				$target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+});
+
 function genHash(data) {
 	data = data.split("").reverse().join("").substring(0, data.length - 1);
 	// TODO get key
