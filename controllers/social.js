23bcdc146709a85495dada3af583ea4018b9f769
Artur Karapetov <dikkini@gmail.com>
2015-12-17 16:29:43 +0300

    forum functions

--- controllers/social.js
+++ controllers/social.js
@@ -6,13 +6,63 @@ var express = require('express')
 	, config = require('../libs/config');
 
 router.get('/', function (req, res, next) {
-	log.debug("Get all categories");
-	db.query(config.get("sql:social:get_all_active_categories"), [], function (err, result) {
-		log.debug(result);
-		if (err) {
-			log.error("Get all categories error: ", err);
+	log.debug("Building index page");
+	async.waterfall([
+		function (callback) {
+			db.query(config.get("sql:social:get_all_active_categories"), [], function (err, result) {
+				log.debug(result);
+				if (err) {
+					res.status(err.status || 500);
+					log.error('Internal error(%d): %s',res.statusCode,err.message);
+					return res.render('errors/500');
+				}
+				var categories = result.rows;
+				callback(null, categories);
+			});
+		},
+		function (categories, callback) {
+			async.forEachOf(categories, function (value, key, callback) {
+				db.query(config.get("sql:social:count_category_topics"), [value.id], function (err, result) {
+					log.debug(result);
+					if (err) {
+						log.error(err);
+						value.topicsCount = 0;
+					} else {
+						value.topicsCount = result.rows[0].count;
+					}
+
+					db.query(config.get("sql:social:count_category_comments"), [value.id], function (err, result) {
+						log.debug(result);
+						if (err) {
+							log.error(err);
+							value.commentsCount = 0;
+						} else {
+							value.commentsCount = result.rows[0].count;
+						}
+						callback();
+					});
+				});
+			}, function (err) {
+				if (err) console.error(err.message);
+				// configs is now a map of JSON data
+				callback(null, categories);
+			});
+		}, function (categories, callback) {
+			log.debug("Get recent gratitudes topics");
+			db.query(config.get("sql:social:get_recent_gratitudes_topics"), [], function (err, result) {
+				var pageData = {};
+				log.debug(result);
+				if (err) {
+					log.error(err);
+				}
+				pageData.gratitudes = result.rows;
+				pageData.categories = categories;
+
+				callback(null, pageData)
+			});
 		}
-		return res.render('social/index', {categories:result.rows});
+	], function(err, result) {
+		return res.render('social/index', {pageData:result});
 	});
 });
 
@@ -63,63 +113,105 @@ router.post('/getTopicsByCategory', function (req, res, next) {
 
 router.get('/category/:id', function (req, res, next) {
 	var categoryId = req.params.id;
-	log.debug("Get all topics for category id: " + categoryId);
-	db.query(config.get("sql:social:get_category_by_id"), [categoryId], function (err, result) {
+	log.debug("Building index page");
+	async.waterfall([
+		function (callback) {
+			db.query(config.get("sql:social:get_all_topics_by_category_id"), [categoryId], function (err, result) {
+				log.debug(result);
+				if (err) {
+					res.status(err.status || 500);
+					log.error('Internal error(%d): %s',res.statusCode,err.message);
+					return res.render('errors/500');
+				}
+				var topics = result.rows;
+				callback(null, topics);
+			});
+		},
+		function (topics, callback) {
+			async.forEachOf(topics, function (value, key, callback) {
+				db.query(config.get("sql:social:count_topic_comments"), [value.id], function (err, result) {
+					log.debug(result);
+					if (err) {
+						log.error(err);
+						value.commentsCount = 0;
+					} else {
+						value.commentsCount = result.rows[0].count;
+					}
 
-		log.debug(result);
-		if (err) {
-			log.error(err);
-			res.status(err.status || 500);
-			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			return res.render('errors/500');
+					callback();
+				});
+			}, function (err) {
+				if (err) console.error(err.message);
+				// data is ready, call callback of waterfall
+				callback(null, topics);
+			});
+		}, function (topics, callback) {
+			log.debug("Get recent gratitudes topics");
+			db.query(config.get("sql:social:get_recent_gratitudes_topics"), [], function (err, result) {
+				var pageData = {};
+				log.debug(result);
+				if (err) {
+					log.error(err);
+				}
+				pageData.gratitudes = result.rows;
+				pageData.topics = topics;
+
+				callback(null, pageData)
+			});
 		}
-		return res.render('social/category', {category:result.rows[0]});
+	], function(err, result) {
+		return res.render('social/category', {pageData:result});
 	});
 });
 
 router.get('/topic/:topicId', function (req, res, next) {
 	var topicId = req.params.topicId;
-	db.query(config.get("sql:social:get_topic_by_id"), [topicId], function (err, result) {
+	log.debug("Building topic page");
+	async.waterfall([
+		function (callback) {
+			db.query(config.get("sql:social:get_topic_by_id"), [topicId], function (err, result) {
 
-		log.debug(result);
-		if (err) {
-			log.error(err);
-			res.status(err.status || 500);
-			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			return res.render('errors/500');
+				log.debug(result);
+				if (err) {
+					log.error(err);
+					res.status(err.status || 500);
+					log.error('Internal error(%d): %s',res.statusCode,err.message);
+					return res.render('errors/500');
+				}
+
+				var topic = result.rows[0];
+
+				callback(null, topic)
+			});
+		}, function (topic, callback) {
+			log.debug("get_all_comments_for_topic_order_by_createddate");
+			db.query(config.get("sql:social:get_all_comments_for_topic_order_by_createddate"), [topic.id], function (err, result) {
+				log.debug(result);
+				if (err) {
+					log.error(err);
+				}
+				var comments = result.rows;
+
+				callback(null, topic, comments)
+			});
+		}, function (topic, comments, callback) {
+			log.debug("Get recent gratitudes topics");
+			db.query(config.get("sql:social:get_recent_gratitudes_topics"), [], function (err, result) {
+				var pageData = {};
+				log.debug(result);
+				if (err) {
+					log.error(err);
+				}
+				pageData.topic = topic;
+				pageData.comments = comments;
+				pageData.gratitudes = result.rows;
+
+				callback(null, pageData)
+			});
 		}
-		var topic = result.rows[0];
-		topic.since = timeSince(topic.createddate);
-		return res.render('social/topic', {topic:topic});
+	], function(err, result) {
+		return res.render('social/topic', {pageData:result});
 	});
 });
-
-function timeSince(date) {
-
-	var seconds = Math.floor((new Date() - date) / 1000);
-
-	var interval = Math.floor(seconds / 31536000);
-
-	if (interval > 1) {
-		return interval + " years ago";
-	}
-	interval = Math.floor(seconds / 2592000);
-	if (interval > 1) {
-		return interval + " months ago";
-	}
-	interval = Math.floor(seconds / 86400);
-	if (interval > 1) {
-		return interval + " days ago";
-	}
-	interval = Math.floor(seconds / 3600);
-	if (interval > 1) {
-		return interval + " hours ago";
-	}
-	interval = Math.floor(seconds / 60);
-	if (interval > 1) {
-		return interval + " minutes ago";
-	}
-	return Math.floor(seconds) + " seconds ago";
-};
 
 module.exports = router;
--- public/js/social/main.js
+++ public/js/social/main.js
@@ -1,26 +1,5 @@
+$(document).ready(function() {
+	'use strict';
 
-var $body = $("body");
-
-function renderTopic(topic, $container) {
-	var topicDateStr = topic.createddate;
-	var topicDate = new Date(topicDateStr);
-	var since = timeSince(topicDate);
-
-	var html = '<a data-topicid="' + topic.id + '" href="/social/topic/' + topic.id + '" class="topic list-group-item">' +
-		'<div class="row">' +
-		'<div class="col-xs-8">' +
-		'<h4 class="list-group-item-heading topic-subject">' + topic.subject + '</h4>' +
-		'</div>' +
-		'<div class="col-xs-4">' +
-		'<small>' + since + '</small>' +
-		'</div>' +
-		'</div>' +
-		'<p class="list-group-item-text topic-text">' + topic.text + '</p>' +
-		'</a>';
-
-	$container.append(html);
-}
-
-$body.on("click", ".topic", function() {
-
+	$("#social-navigation").addClass("active");
 });
