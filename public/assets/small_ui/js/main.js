1c45efea5b0197facba287d00baab8e58f48e8bb
Artur Karapetov <dikkini@gmail.com>
2015-12-18 11:53:17 +0300

    noty plugin and topic markup fixes

--- public/assets/small_ui/js/main.js
+++ public/assets/small_ui/js/main.js
@@ -4,6 +4,35 @@ $(document).ready(function() {
 	var doc = document.documentElement;
 	doc.setAttribute('data-useragent', navigator.userAgent);
 
+	$.noty.defaults = {
+		layout: 'top',
+		theme: 'relax', // or 'relax'
+		type: 'alert',
+		text: '', // can be html or string
+		dismissQueue: true, // If you want to use queue feature set this true
+		template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+		animation: {
+			open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+			close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+			easing: 'swing',
+			speed: 500 // opening & closing animation speed
+		},
+		timeout: 3000, // delay for closing event. Set false for sticky notifications
+		force: false, // adds notification to the beginning of queue when set to true
+		modal: false,
+		maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+		killer: false, // for close all notifications before show
+		closeWith: ['click'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+		callback: {
+			onShow: function() {},
+			afterShow: function() {},
+			onClose: function() {},
+			afterClose: function() {},
+			onCloseClick: function() {},
+		},
+		buttons: false // an array of buttons
+	};
+
 	var ALL_LOADERS_COUNT = 0;
 
 	var SESSIONID;
@@ -372,8 +401,27 @@ $(document).ready(function() {
 	});
 
 	$("#send-feedback").click(function() {
+		var name = $("#feedback-name").val();
 		var email = $("#feedback-email").val();
 		var feedback = $("#feedback-text").val();
+
+		if ($.trim(name) == "") {
+			noty({
+				text: 'Enter your name please!'
+			});
+			return;
+		} else if ($.trim(email) == "") {
+			noty({
+				text: 'Enter email please!'
+			});
+			return;
+		} else if ($.trim(feedback) == "") {
+			noty({
+				text: 'Enter feedback text please!'
+			});
+			return;
+		}
+
 		$("#feedback-modal").modal("hide");
 		dontBlock = true;
 
@@ -382,6 +430,7 @@ $(document).ready(function() {
 			type: "POST",
 			dataType: "JSON",
 			data: {
+				"name": name,
 				"email": email,
 				"feedback": feedback,
 				"sessionId": SESSIONID
--- public/js/social/topic.js
+++ public/js/social/topic.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
 	'use strict';
 
+	$.noty.defaults = {
+		layout: 'top',
+		theme: 'relax', // or 'relax'
+		type: 'alert',
+		text: '', // can be html or string
+		dismissQueue: true, // If you want to use queue feature set this true
+		template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+		animation: {
+			open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+			close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+			easing: 'swing',
+			speed: 500 // opening & closing animation speed
+		},
+		timeout: 3000, // delay for closing event. Set false for sticky notifications
+		force: false, // adds notification to the beginning of queue when set to true
+		modal: false,
+		maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+		killer: false, // for close all notifications before show
+		closeWith: ['click'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+		callback: {
+			onShow: function() {},
+			afterShow: function() {},
+			onClose: function() {},
+			afterClose: function() {},
+			onCloseClick: function() {},
+		},
+		buttons: false // an array of buttons
+	};
+
 	$("#social-navigation").addClass("active");
 
 	$("#hide-comments-btn").click(function() {
@@ -15,6 +44,18 @@ $(document).ready(function() {
 	$("#add-comment-btn").click(function() {
 		var commentName = $("#comment-name").val();
 		var commentText = $("#comment-text").val();
+		if ($.trim(commentName) == "") {
+			noty({
+				text: 'Enter your name please!'
+			});
+			return;
+		}
+		if ($.trim(commentText) == "") {
+			noty({
+				text: 'Enter comment text please!'
+			});
+			return;
+		}
 		var topicId = $("#topicTable").attr("data-topicid");
 
 		$.ajax({
