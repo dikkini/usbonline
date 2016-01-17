0e5cb6fdc6018032181bf67fc44218656b91ea29
Artur Karapetov <dikkini@gmail.com>
2016-01-17 13:06:16 +0300

    catch error on downloading file

--- controllers/download.js
+++ controllers/download.js
@@ -6,14 +6,23 @@ var express = require('express')
 	, path = require('path');
 
 router.get('/application', function (req, res, next) {
-	db.query(config.get("sql:stats:update_downloadportable"), [], function (err, result) {
-		log.debug(result);
-		if (err) {
-			log.error(err);
-		}
-	});
 	var file = '/opt/bootline/BootLine.exe';
-	res.download(file); // Set disposition and send it.
+	res.download(file, function (err) {
+		if (err) {
+			// Check if headers have been sent
+			if (res.headersSent) {
+				db.query(config.get("sql:stats:update_downloadportable"), [], function (err, result) {
+					log.debug(result);
+					if (err) {
+						log.error(err);
+					}
+				});
+			} else {
+				log.debug("Not found file : " + file);
+				return res.sendStatus(404); // 404, maybe 500 depending on err
+			}
+		}
+	}); // Set disposition and send it.
 });
 
 
