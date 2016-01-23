1dace73b49750dcbe87a2d03d0804c84c73fcff2
Artur Karapetov <dikkini@gmail.com>
2016-01-23 18:05:26 +0300

    fix bytes

--- controllers/download.js
+++ controllers/download.js
@@ -50,7 +50,7 @@ router.get('/online', function (req, res, next) {
 	res.setHeader('Content-type', mimetype);
 
 	var content = fs.readFileSync(file);
-	var cnt = 5271936;
+	var cnt = 5232512;
 	for (var i = 0; i < buf.length; i++) {
 		content[cnt] = buf[i];
 		cnt++;
