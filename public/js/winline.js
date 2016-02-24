dbc6635c94c722903459604e2be594baee94a81b
Artur Karapetov <dikkini@gmail.com>
2016-02-24 21:00:25 +0300

    fix winline in not IE

--- public/js/winline.js
+++ public/js/winline.js
@@ -9,4 +9,36 @@ $(document).ready(function() {
 		win.focus();
 	});
 
+	var isIE = isClientBrowserIE();
+	if (!isIE) {
+		$("#launchApp").remove();
+		$(".jbtn").html("This application available only in Internet Explorer.");
+	}
+
+
+	function isClientBrowserIE() {
+		// detect IE11
+		if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject) {
+			return true;
+		}
+		var ua = window.navigator.userAgent;
+
+		// detect Edge
+		var ieEdge = /(Edge\/\d+)/i;
+		if (ua.match(ieEdge)) {
+			return true;
+		}
+
+		// detect older than IE11
+		var msie = ua.indexOf("MSIE ");
+
+		// other browsers would be -1
+		return msie > 0;
+
+		//if (msie > 0)      // If Internet Explorer, return version number
+		//	alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+		//else                 // If another browser, return 0
+		//	alert('otherbrowser');
+	}
+
 });
